4eaee11b81ce266f1bb7363e6e24ebfdc0afa537
Nicolas Hirsig <n.hirsig@cloudtec.ch>
2023-12-02 09:33:04 +0100

    Make small changes to work with gpt4all-api, sending curl request to local-server

--- lua/chatgpt/api.lua
+++ lua/chatgpt/api.lua
@@ -6,88 +6,104 @@ local Api = {}
 
 -- API URL
 Api.BASE_URL = "http://localhost:4891"
-Api.COMPLETIONS_URL = Api.BASE_URL .. "/v1/completions"
+Api.COMPLETIONS_URL = Api.BASE_URL .. "/v1/completions/"
 Api.CHAT_COMPLETIONS_URL = Api.BASE_URL .. "/v1/chat/completions"
 Api.EDITS_URL = Api.BASE_URL .. "/v1/edits"
 
 function Api.completions(custom_params, cb)
-  local params = vim.tbl_extend("keep", custom_params, Config.options.openai_params)
-  Api.make_call(Api.COMPLETIONS_URL, params, cb)
+    local params = vim.tbl_extend("keep", custom_params, Config.options.openai_params)
+    Api.make_call(Api.COMPLETIONS_URL, params, cb)
 end
 
 function Api.chat_completions(custom_params, cb)
-  local params = vim.tbl_extend("keep", custom_params, Config.options.openai_params)
-  Api.make_call(Api.CHAT_COMPLETIONS_URL, params, cb)
+    local params = vim.tbl_extend("keep", custom_params, Config.options.openai_params)
+    Api.make_call(Api.CHAT_COMPLETIONS_URL, params, cb)
 end
 
 function Api.edits(custom_params, cb)
-  local params = vim.tbl_extend("keep", custom_params, Config.options.openai_edit_params)
-  Api.make_call(Api.EDITS_URL, params, cb)
+    local params = vim.tbl_extend("keep", custom_params, Config.options.openai_edit_params)
+    Api.make_call(Api.EDITS_URL, params, cb)
 end
 
 function Api.make_call(url, params, cb)
-  TMP_MSG_FILENAME = os.tmpname()
-  local f = io.open(TMP_MSG_FILENAME, "w+")
-  if f == nil then
-    vim.notify("Cannot open temporary message file: " .. TMP_MSG_FILENAME, vim.log.levels.ERROR)
-    return
-  end
-  f:write(vim.fn.json_encode(params))
-  f:close()
-  Api.job = job
-    :new({
-      command = "curl",
-      args = {
-        url,
-        "-H",
-        "Content-Type: application/json",
-        "-d",
-        "@" .. TMP_MSG_FILENAME,
-      },
-      on_exit = vim.schedule_wrap(function(response, exit_code)
-        Api.handle_response(response, exit_code, cb)
-      end),
-    })
-    :start()
+    -- the OpenAI API only looks at conversation (messages), the gpt4all API instead looks at 'prompt'
+
+    if params["messages"] ~= nil then
+        params.prompt = params.messages[#params.messages].content
+    end
+
+    TMP_MSG_FILENAME = os.tmpname()
+    local f = io.open(TMP_MSG_FILENAME, "w+")
+    if f == nil then
+        vim.notify("Cannot open temporary message file: " .. TMP_MSG_FILENAME, vim.log.levels.ERROR)
+        return
+    end
+    f:write(vim.fn.json_encode(params))
+    f:close()
+
+    -- Set the API endpoint and data
+    local api_endpoint = "http://localhost:4891/v1/completions/"
+    local api_key = "not_needed_for_a_local_LLM"
+    Api.job = job
+        :new({
+            command = 'curl',
+            args = {
+                '-X', 'POST',
+                '-H', 'Content-Type: application/json',
+                '-H', 'Authorization: Bearer ' .. api_key,
+                '-d', '@' .. TMP_MSG_FILENAME,
+                api_endpoint
+            },
+            on_exit = vim.schedule_wrap(function(response, exit_code)
+                Api.handle_response(response, exit_code, cb)
+            end),
+            on_stdout = function(_, data)
+                print('Stdout:', data)
+            end,
+            on_stderr = function(_, data)
+                print('Stderr:', data)
+            end,
+        })
+        :start()
 end
 
 Api.handle_response = vim.schedule_wrap(function(response, exit_code, cb)
-  os.remove(TMP_MSG_FILENAME)
-  if exit_code ~= 0 then
-    vim.notify("An Error Occurred ...", vim.log.levels.ERROR)
-    cb("ERROR: API Error")
-  end
-
-  local result = table.concat(response:result(), "\n")
-  local json = vim.fn.json_decode(result)
-  if json == nil then
-    cb("No Response.")
-  elseif json.error then
-    cb("// API ERROR: " .. json.error.message)
-  else
-    local message = json.choices[1].message
-    if message ~= nil then
-      local response_text = json.choices[1].message.content
-      if type(response_text) == "string" and response_text ~= "" then
-        cb(response_text, json.usage)
-      else
-        cb("...")
-      end
-    else
-      local response_text = json.choices[1].text
-      if type(response_text) == "string" and response_text ~= "" then
-        cb(response_text, json.usage)
-      else
-        cb("...")
-      end
+    -- os.remove(TMP_MSG_FILENAME)
+    if exit_code ~= 0 then
+        vim.notify("An Error Occurred ...", vim.log.levels.ERROR)
+        cb("ERROR: API Error")
     end
-  end
+
+    local result = table.concat(response:result(), "\n")
+    local json = vim.fn.json_decode(result)
+    if json == nil then
+        cb("No Response.")
+    elseif json.error then
+        cb("// API ERROR: " .. json.error.message)
+    else
+        local message = json.choices[1].message
+        if message ~= nil then
+            local response_text = json.choices[1].message.content
+            if type(response_text) == "string" and response_text ~= "" then
+                cb(response_text, json.usage)
+            else
+                cb("...")
+            end
+        else
+            local response_text = json.choices[1].text
+            if type(response_text) == "string" and response_text ~= "" then
+                cb(response_text, json.usage)
+            else
+                cb("...")
+            end
+        end
+    end
 end)
 
 function Api.close()
-  if Api.job then
-    job:shutdown()
-  end
+    if Api.job then
+        job:shutdown()
+    end
 end
 
 function Api.setup()
--- lua/chatgpt/flows/actions/chat/init.lua
+++ lua/chatgpt/flows/actions/chat/init.lua
@@ -31,110 +31,111 @@ local STRATEGY_DISPLAY = "display"
 local STRATEGY_QUICK_FIX = "quick_fix"
 
 function ChatAction:init(opts)
-  self.super:init(opts)
-  self.params = opts.params or {}
-  self.template = opts.template or "{{input}}"
-  self.variables = opts.variables or {}
-  self.strategy = opts.strategy or STRATEGY_APPEND
+    self.super:init(opts)
+    self.params = opts.params or {}
+    self.template = opts.template or "{{input}}"
+    self.variables = opts.variables or {}
+    self.strategy = opts.strategy or STRATEGY_APPEND
 end
 
 function ChatAction:render_template()
-  local input = self.strategy == STRATEGY_QUICK_FIX and self:get_selected_text_with_line_numbers()
+    local input = self.strategy == STRATEGY_QUICK_FIX and self:get_selected_text_with_line_numbers()
     or self:get_selected_text()
-  local data = {
-    filetype = self:get_filetype(),
-    input = input,
-  }
-  data = vim.tbl_extend("force", {}, data, self.variables)
-  local result = self.template
-  for key, value in pairs(data) do
-    result = result:gsub("{{" .. key .. "}}", value)
-  end
-  return result
+    local data = {
+        filetype = self:get_filetype(),
+        input = input,
+    }
+    data = vim.tbl_extend("force", {}, data, self.variables)
+    local result = self.template
+    for key, value in pairs(data) do
+        result = result:gsub("{{" .. key .. "}}", value)
+    end
+    return result
 end
 
 function ChatAction:get_params()
-  local messages = self.params.messages or {}
-  local message = {
-    role = "user",
-    content = self:render_template(),
-  }
-  table.insert(messages, message)
-  return vim.tbl_extend("force", Config.options.openai_params, self.params, { messages = messages })
+    local messages = self.params.messages or {}
+    local message = {
+        role = "user",
+        content = self:render_template(),
+    }
+    table.insert(messages, message)
+
+    return vim.tbl_extend("force", Config.options.openai_params, self.params, { messages = messages }, prompt)
 end
 
 function ChatAction:run()
-  vim.schedule(function()
-    self:set_loading(true)
+    vim.schedule(function()
+        self:set_loading(true)
 
-    local params = self:get_params()
-    Api.chat_completions(params, function(answer, usage)
-      self:on_result(answer, usage)
+        local params = self:get_params()
+        Api.chat_completions(params, function(answer, usage)
+            self:on_result(answer, usage)
+        end)
     end)
-  end)
 end
 
 function ChatAction:on_result(answer, usage)
-  vim.schedule(function()
-    self:set_loading(false)
+    vim.schedule(function()
+        self:set_loading(false)
 
-    local bufnr = self:get_bufnr()
-    if self.strategy == STRATEGY_PREPEND then
-      answer = answer .. "\n" .. self:get_selected_text()
-    elseif self.strategy == STRATEGY_APPEND then
-      answer = self:get_selected_text() .. "\n\n" .. answer .. "\n"
-    end
-    local lines = Utils.split_string_by_line(answer)
-    local _, start_row, start_col, end_row, end_col = self:get_visual_selection()
+        local bufnr = self:get_bufnr()
+        if self.strategy == STRATEGY_PREPEND then
+            answer = answer .. "\n" .. self:get_selected_text()
+        elseif self.strategy == STRATEGY_APPEND then
+            answer = self:get_selected_text() .. "\n\n" .. answer .. "\n"
+        end
+        local lines = Utils.split_string_by_line(answer)
+        local _, start_row, start_col, end_row, end_col = self:get_visual_selection()
 
-    if self.strategy == STRATEGY_DISPLAY then
-      local PreviewWindow = require("chatgpt.common.preview_window")
-      local popup = PreviewWindow({
+        if self.strategy == STRATEGY_DISPLAY then
+            local PreviewWindow = require("chatgpt.common.preview_window")
+            local popup = PreviewWindow({
 
-        border = {
-          style = "single",
-          text = {
-            top = " " .. self.opts.title .. " ",
-            top_align = "left",
-          },
-        },
-        relative = {
-          type = "buf",
-          position = {
-            row = start_row,
-            col = start_col,
-          },
-        },
-      })
-      vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, lines)
-      popup:mount()
-    elseif self.strategy == STRATEGY_EDIT then
-      Edits.edit_with_instructions(lines, bufnr, { self:get_visual_selection() })
-    elseif self.strategy == STRATEGY_QUICK_FIX then
-      if #lines == 1 and lines[1] == "<OK>" then
-        vim.notify("Your Code looks fine, no issues.", vim.log.levels.INFO)
-        return
-      end
+                border = {
+                    style = "single",
+                    text = {
+                        top = " " .. self.opts.title .. " ",
+                        top_align = "left",
+                    },
+                },
+                relative = {
+                    type = "buf",
+                    position = {
+                        row = start_row,
+                        col = start_col,
+                    },
+                },
+            })
+            vim.api.nvim_buf_set_lines(popup.bufnr, 0, 1, false, lines)
+            popup:mount()
+        elseif self.strategy == STRATEGY_EDIT then
+            Edits.edit_with_instructions(lines, bufnr, { self:get_visual_selection() })
+        elseif self.strategy == STRATEGY_QUICK_FIX then
+            if #lines == 1 and lines[1] == "<OK>" then
+                vim.notify("Your Code looks fine, no issues.", vim.log.levels.INFO)
+                return
+            end
 
-      local entries = {}
-      for _, line in ipairs(lines) do
-        local lnum, text = line:match("(%d+):(.*)")
+            local entries = {}
+            for _, line in ipairs(lines) do
+                local lnum, text = line:match("(%d+):(.*)")
 
-        local entry = { filename = vim.fn.expand("%:p"), lnum = tonumber(lnum), text = text }
-        table.insert(entries, entry)
-      end
-      vim.fn.setqflist(entries)
-      vim.cmd(Config.options.show_quickfixes_cmd)
-    else
-      vim.api.nvim_buf_set_text(bufnr, start_row - 1, start_col - 1, end_row - 1, end_col, lines)
+                local entry = { filename = vim.fn.expand("%:p"), lnum = tonumber(lnum), text = text }
+                table.insert(entries, entry)
+            end
+            vim.fn.setqflist(entries)
+            vim.cmd(Config.options.show_quickfixes_cmd)
+        else
+            vim.api.nvim_buf_set_text(bufnr, start_row - 1, start_col - 1, end_row - 1, end_col, lines)
 
-      -- set the cursor onto the answer
-      if self.strategy == STRATEGY_APPEND then
-        local target_line = end_row + 3
-        vim.api.nvim_win_set_cursor(0, { target_line, 0 })
-      end
-    end
-  end)
+            -- set the cursor onto the answer
+            if self.strategy == STRATEGY_APPEND then
+                local target_line = end_row + 3
+                vim.api.nvim_win_set_cursor(0, { target_line, 0 })
+            end
+        end
+    end)
 end
 
 return ChatAction
